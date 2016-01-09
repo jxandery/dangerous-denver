c0cd9c64e2970ce256cc7eb20a0877e6d4ecf5aa
Jack Yeh <jack.yeh@gmail.com>
2016-01-08 17:33:43 -0700

    add functionality: document the time that each method requires

--- lib/ruby/index.rb
+++ lib/ruby/index.rb
@@ -1,29 +1,37 @@
 require 'csv'
 require 'benchmark'
 
-def create_records(filename)
+def mark_time(search, search_name)
   beginning_time = Time.now
+  search
+  end_time = Time.now
+  puts "#{search_name} completed in #{(end_time - beginning_time)} seconds"
+end
+
+def parse_csv(filename)
   records = []
   csv = CSV.open("../../data/#{filename}.csv", headers: true, header_converters: :symbol)
-  csv.each do |line|
-    records << line.to_h
-  end
-
-  end_time = Time.now
-  puts "#{records.length} created"
-  puts "Time elapsed #{(end_time - beginning_time)} seconds"
+  csv.each { |line| records << line.to_h }
   records
 end
 
 def find_top_locations(filename, category, x)
   crime_count = []
-  records = create_records(filename)
+  records = parse_csv(filename)
   neighborhoods = records.group_by { |header| header[category] }.values
   neighborhoods.each { |n| crime_count.push( [n.count, n.first[category]] ) }
-  crime_count.sort.reverse.first(x)
+  puts crime_count.sort.reverse.first(x)
 end
 
-puts find_top_locations('crime', :neighborhood_id, 5)
-puts find_top_locations('traffic-accidents', :neighborhood_id, 5)
-puts find_top_locations('traffic-accidents', :incident_address, 5)
+parse_crime       = parse_csv('crime')
+parse_accidents   = parse_csv('traffic-accidents')
+crime             = find_top_locations('crime', :neighborhood_id, 5)
+accidents         = find_top_locations('traffic-accidents', :neighborhood_id, 5)
+dangerous_corners = find_top_locations('traffic-accidents', :incident_address, 5)
+
+mark_time(parse_accidents, 'Read accidents CSV')
+mark_time(parse_crime, 'Read crime CSV')
+mark_time(crime, 'Top crime locations search')
+mark_time(accidents, 'Top accidents locations search')
+mark_time(dangerous_corners, 'Most dangerous corners locations search')
 
